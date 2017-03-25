e539aedbcaf5e49ad2c8f0fb94406dfa2a1437cd
Ahmad Shahab <ashahab28@gmail.com>
2017-03-25 13:35:41 +0700

    initialize microrouter

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
-const {send} = require('micro')
+const { router, get, post } = require('microrouter')
+const { json, send } = require('micro')
+
 const db = require('../util/db')
 
-module.exports = async (req, res) => {
-  // TODO: remove name and get correct data
-  const name = await db.ref('meta').once('value', snapshot => snapshot.val())
-  send(res, 200, {caprica: 'online', name})
+const hello = async (req, res) =>
+  send(res, 200, await Promise.resolve(`Hello ${req.params.who}`))
+
+const user = async (req, res) => {
+  const body = await json(req)
+  send(res, 200, body)
 }
+
+module.exports = router(
+  get('/hello/:who', hello),
+  post('/user', user)
+)
